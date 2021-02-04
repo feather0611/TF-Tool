65610c87977a4a93434283ca3ab595b2f9ab1bc8
feather0611 <feather0611@icloud.com>
2021-02-04 20:05:35 +0800

    -Fix some UI setting. -Make directory select button works.
    -修改部分UI設置
    -讓選取資料夾的按鈕可以運作

--- ui.py
+++ ui.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'mainview.ui'
+# Form implementation generated from reading ui file 'view.ui'
 #
 # Created by: PyQt5 UI code generator 5.15.2
 #
@@ -9,7 +9,8 @@
 
 
 from PyQt5 import QtCore, QtGui, QtWidgets
-
+from PyQt5.QtWidgets import QFileDialog
+from os.path import expanduser
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
@@ -30,52 +31,59 @@ class Ui_MainWindow(object):
         self.label_2.setFont(font)
         self.label_2.setLayoutDirection(QtCore.Qt.LeftToRight)
         self.label_2.setObjectName("label_2")
-        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton.setGeometry(QtCore.QRect(450, 21, 71, 21))
-        self.pushButton.setObjectName("pushButton")
-        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_2.setGeometry(QtCore.QRect(450, 60, 71, 21))
-        self.pushButton_2.setObjectName("pushButton_2")
+        self.Origin_context_selButton = QtWidgets.QPushButton(self.centralwidget)
+        self.Origin_context_selButton.setGeometry(QtCore.QRect(450, 16, 71, 31))
+        self.Origin_context_selButton.setObjectName("Origin_context_selButton")
+        self.Split_result_selButton = QtWidgets.QPushButton(self.centralwidget)
+        self.Split_result_selButton.setGeometry(QtCore.QRect(450, 56, 71, 31))
+        self.Split_result_selButton.setObjectName("Split_result_selButton")
         self.label_3 = QtWidgets.QLabel(self.centralwidget)
         self.label_3.setGeometry(QtCore.QRect(20, 100, 111, 20))
         font = QtGui.QFont()
         font.setFamily("Arial")
         self.label_3.setFont(font)
         self.label_3.setObjectName("label_3")
-        self.pushButton_3 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_3.setGeometry(QtCore.QRect(450, 101, 71, 21))
-        self.pushButton_3.setObjectName("pushButton_3")
+        self.TF_output__selButton = QtWidgets.QPushButton(self.centralwidget)
+        self.TF_output__selButton.setGeometry(QtCore.QRect(450, 96, 71, 31))
+        self.TF_output__selButton.setObjectName("TF_output__selButton")
         self.Origin_context_dir = QtWidgets.QLineEdit(self.centralwidget)
-        self.Origin_context_dir.setEnabled(False)
+        self.Origin_context_dir.setEnabled(True)
         self.Origin_context_dir.setGeometry(QtCore.QRect(140, 20, 301, 21))
+        self.Origin_context_dir.setReadOnly(True)
         self.Origin_context_dir.setObjectName("Origin_context_dir")
         self.Split_result_dir = QtWidgets.QLineEdit(self.centralwidget)
-        self.Split_result_dir.setEnabled(False)
+        self.Split_result_dir.setEnabled(True)
         self.Split_result_dir.setGeometry(QtCore.QRect(140, 60, 301, 21))
+        self.Split_result_dir.setReadOnly(True)
         self.Split_result_dir.setObjectName("Split_result_dir")
         self.TF_output_dir = QtWidgets.QLineEdit(self.centralwidget)
-        self.TF_output_dir.setEnabled(False)
+        self.TF_output_dir.setEnabled(True)
         self.TF_output_dir.setGeometry(QtCore.QRect(140, 100, 301, 21))
+        self.TF_output_dir.setReadOnly(True)
         self.TF_output_dir.setObjectName("TF_output_dir")
-        self.plainTextEdit = QtWidgets.QPlainTextEdit(self.centralwidget)
-        self.plainTextEdit.setEnabled(False)
-        self.plainTextEdit.setGeometry(QtCore.QRect(20, 170, 501, 101))
-        self.plainTextEdit.setObjectName("plainTextEdit")
+        self.hintBox = QtWidgets.QPlainTextEdit(self.centralwidget)
+        self.hintBox.setEnabled(True)
+        self.hintBox.setGeometry(QtCore.QRect(20, 170, 501, 101))
+        self.hintBox.setReadOnly(True)
+        self.hintBox.setObjectName("hintBox")
         self.label_4 = QtWidgets.QLabel(self.centralwidget)
         self.label_4.setGeometry(QtCore.QRect(20, 140, 111, 20))
         font = QtGui.QFont()
         font.setFamily("Arial")
         self.label_4.setFont(font)
         self.label_4.setObjectName("label_4")
-        self.pushButton_4 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_4.setGeometry(QtCore.QRect(410, 280, 113, 32))
-        self.pushButton_4.setObjectName("pushButton_4")
+        self.runButton = QtWidgets.QPushButton(self.centralwidget)
+        self.runButton.setGeometry(QtCore.QRect(410, 280, 113, 32))
+        self.runButton.setObjectName("runButton")
         MainWindow.setCentralWidget(self.centralwidget)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
         MainWindow.setStatusBar(self.statusbar)
 
         self.retranslateUi(MainWindow)
+        self.Origin_context_selButton.clicked.connect(lambda: self.chooseDir('origin'))
+        self.Split_result_selButton.clicked.connect(lambda: self.chooseDir('split'))
+        self.TF_output__selButton.clicked.connect(lambda: self.chooseDir('TF'))
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
@@ -83,9 +91,29 @@ class Ui_MainWindow(object):
         MainWindow.setWindowTitle(_translate("MainWindow", "詞頻計算程式"))
         self.label.setText(_translate("MainWindow", "原始文本目錄"))
         self.label_2.setText(_translate("MainWindow", "斷詞結果輸出目錄"))
-        self.pushButton.setText(_translate("MainWindow", "選取"))
-        self.pushButton_2.setText(_translate("MainWindow", "選取"))
+        self.Origin_context_selButton.setText(_translate("MainWindow", "選取"))
+        self.Split_result_selButton.setText(_translate("MainWindow", "選取"))
         self.label_3.setText(_translate("MainWindow", "詞頻結果輸出目錄"))
-        self.pushButton_3.setText(_translate("MainWindow", "選取"))
+        self.TF_output__selButton.setText(_translate("MainWindow", "選取"))
         self.label_4.setText(_translate("MainWindow", "訊息提示"))
-        self.pushButton_4.setText(_translate("MainWindow", "執行"))
+        self.runButton.setText(_translate("MainWindow", "執行"))
+
+    def chooseDir(self, target):
+        if target=='origin':
+            self.Origin_context_dir.setText(str(QFileDialog.getExistingDirectory(
+                None,
+                "Open a folder",
+                expanduser("~"),
+                QFileDialog.ShowDirsOnly))+'/')
+        elif target=='split':
+            self.Split_result_dir.setText(str(QFileDialog.getExistingDirectory(
+                None,
+                "Open a folder",
+                expanduser("~"),
+                QFileDialog.ShowDirsOnly))+'/')
+        elif target=='TF':
+            self.TF_output_dir.setText(str(QFileDialog.getExistingDirectory(
+                None,
+                "Open a folder",
+                expanduser("~"),
+                QFileDialog.ShowDirsOnly))+'/')
